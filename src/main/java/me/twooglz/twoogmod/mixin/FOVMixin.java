b9daeba929354becbf0aff3be442b95730edd52a
Twooglz <103332410+Twooglz@users.noreply.github.com>
2024-04-04 03:07:18 +0200

    first commit!!!1!1111!

---
+++ src/main/java/me/twooglz/twoogmod/mixin/FOVMixin.java
@@ -0,0 +1,22 @@
+package me.twooglz.twoogmod.mixin;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(GameOptions.class)
+public class FOVMixin {
+    @Final
+    SimpleOption<Integer> fov = new SimpleOption<Integer>("options.fov", SimpleOption.emptyTooltip(), (optionText, value) -> switch (value) {
+        case 70 -> GameOptions.getGenericValueText(optionText, Text.translatable("options.fov.min"));
+        case 110 -> GameOptions.getGenericValueText(optionText, Text.translatable("options.fov.max"));
+        case 160 -> GameOptions.getGenericValueText(optionText, Text.literal("nigga on crack"));
+        default -> GameOptions.getGenericValueText(optionText, value);
+    }, new SimpleOption.ValidatingIntSliderCallbacks(10, 160), Codec.DOUBLE.xmap(value -> (int)(value * 40.0 + 70.0), value -> ((double)value.intValue() - 70.0) / 40.0), 70, value -> MinecraftClient.getInstance().worldRenderer.scheduleTerrainUpdate());
+
+}
+
